6ee33ffc20aa7f80bc83f93ad886eda8aec82df4
Masayuki-Tajima <masatajima12@gmail.com>
2024-11-24 18:59:32 +0900

    タスク一覧表示機能をパーツ化

--- index.php
+++ index.php
@@ -1,17 +1,8 @@
 <?php
-try {
-    include_once("./app/database/connect.php");
+//データベースと接続
+include_once("./app/database/connect.php");
 
-    $task_array = array();
-
-    // 未完了のタスク一覧を取得
-    $sql = "SELECT * FROM tasks WHERE is_done = 0";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute();
-    $task_array = $stmt;
-} catch (PDOException $e) {
-    echo "データベースに接続できませんでした。" . $e->getMessage();
-}
+include("./app/functions/task_get.php");
 
 ?>
 <!DOCTYPE html>
